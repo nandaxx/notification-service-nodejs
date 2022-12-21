e8dc627b1ddf3440f197a4f632530a89d789c486
Fernanda Machado <nandavalle2012@hotmail.com>
2022-12-21 16:29:54 -0300

    validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './createNotificationBody';
 import { PrismaService } from './prisma.service';
 
 @Controller('notificationService')//o @ indica que é um decorator
@@ -13,15 +14,16 @@ export class AppController {
   }
 
   @Post()
-  async create() {
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body
+
     await this.prisma.notification.create({
-      
       data: {
         id: randomUUID(), //gera um id
-        content: 'Você possui uma nova notificação',
-        category: 'social',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
-    });
+     });
   }
 }
